b33d9b3cfdfc5e6715dd021c60e5dc5e6aaa7cfe
joseph-ishak <jishak13@gmail.com>
2019-11-18 13:02:30 -0700

    initial commit

---
+++ src/com/choicehotels/hibernate/entity/Users.java
@@ -0,0 +1,70 @@
+package com.choicehotels.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+	@Id
+	@Column(name = "id")
+	int userId;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "name")
+	private String name;
+
+	
+	public Users() {
+		
+	}
+	
+	public Users(String username, String password, String name) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
